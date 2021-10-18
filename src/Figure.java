5e6c8b700ee7b190d1908bbc782a9c4f30e44692
ZarbiM <margot.laleu@ensea.fr>
2021-10-18 13:21:13 +0200

    version de base

---
+++ src/Figure.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+abstract class Figure {
+
+    public abstract void setBoundingBox (int heightBB, int widthBB);
+    public abstract void draw (Graphics g);
+
+}
--- src/Point.java
+++ src/Point.java
@@ -2,4 +2,33 @@ public class Point {
     private int x;
     private int y;
 
+    public Point(int a, int b){
+        x=a;
+        y=b;
+    }
+
+    public Point(){
+        x=0;
+        y=0;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+       return y;
+    }
+
+    public void setX(int a){
+        x=a;
+    }
+
+    public void setY(int a){
+        y=a;
+    }
+
+    public String toString(){
+        return "("+x+";"+y+")";
+    }
 }
